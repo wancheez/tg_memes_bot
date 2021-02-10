163dd448b2830fd63d9bbd58bc7a4916c96dadf0
wancheez <wancheez@gmail.com>
2021-02-10 12:29:12 +0300

    Fixed wednesday scheduler New pics

--- storage/base_storage.py
+++ storage/base_storage.py
@@ -62,7 +62,7 @@ def schedule_meme_page(bot, funcs, chat_id):
 
 
 def schedule_wednesday(bot, funcs, chat_id):
-    schedule.every().wednesday.at("09:30").do(funcs['wednesday'], chat_id)
+    schedule.every().wednesday.at("09:30").do(funcs['wednesday'], bot, chat_id)
 
 
 def schedule_memes(bot, funcs, chat_id):
